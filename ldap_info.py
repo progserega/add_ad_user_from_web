b5708acaee85447f38c0f732b18e0695fda058cf
progserega <progserega@gmail.com>
2015-10-10 15:57:03 +1000

    вывод списка пользователей

---
+++ ldap_info.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import ldap
+import re
+import config as conf
+
+ldap_url=conf.LDAP_SERVER
+ldap_user=conf.BIND_DN
+ldap_passwd=conf.BIND_PASS
+
+# Some flags for userAccountControl property
+SCRIPT = 1
+ACCOUNTDISABLE = 2
+HOMEDIR_REQUIRED = 8
+PASSWD_NOTREQD = 32
+NORMAL_ACCOUNT = 512
+DONT_EXPIRE_PASSWORD = 65536
+TRUSTED_FOR_DELEGATION = 524288
+PASSWORD_EXPIRED = 8388608
+
+
+def init():
+	global ldap_url
+	global ldap_user
+	global ldap_passwd
+
+	try:
+		ad = ldap.initialize(ldap_url)
+		ad.set_option(ldap.OPT_REFERRALS,0)
+		ad.simple_bind_s(ldap_user,ldap_passwd)
+	except ldap.LDAPError, desc:
+		print("error", desc)
+	return ad
+
+
+def get_computers(ad):
+	comps={}
+	try:
+		#basedn = 'DC=drsk,DC=rao-esv,DC=ru'
+		basedn = conf.base_comp_dn
+		scope = ldap.SCOPE_SUBTREE
+		filterexp = '(&(objectCategory=Computer)(objectClass=Computer)(cn=RSK40*))'
+		attrlist = ['cn','description']
+		results = ad.search_s(basedn, scope, filterexp, attrlist)
+		#print(results)
+	except ldap.LDAPError, desc:
+		print("error", desc)
+
+	for result in results:
+			comp={}
+			comp["name"]=result[1]['cn'][0]
+			comp["full_name"]="%s.drsk.rao-esv.ru" % result[1]['cn'][0].lower()
+			if "description" in result[1]:
+				comp["description"]=result[1]['description'][0]
+			comps[comp["name"]]=comp
+	return comps
+
+def get_users(ad):
+	users={}
+	try:
+		#basedn = 'DC=drsk,DC=rao-esv,DC=ru'
+		basedn = conf.base_user_dn
+		scope = ldap.SCOPE_SUBTREE
+		filterexp = '(sAMAccountName=*)'
+		attrlist = ['sAMAccountName','description','givenName','sn','mail','userWorkstations','initials','displayName','info','displayNamePrintable','canonicalName','ms-DS-UserAccountAutoLocked','msDS-UserAccountDisabled','msDS-UserDontExpirePassword','msDS-UserPasswordExpired','UserAccountControl']
+		#attrlist = ['cn']
+		results = ad.search_s(basedn, scope, filterexp, attrlist)
+	except ldap.LDAPError, desc:
+		print("error", desc)
+
+	for result in results:
+	#		print(result)
+			user={}
+			user["account_name"]=result[1]['sAMAccountName'][0]
+			user["attr"]=int(result[1]['userAccountControl'][0])
+			if "givenName" in result[1]:
+				user["firstname"]=result[1]['givenName'][0]
+			if "description" in result[1]:
+				user["description"]=result[1]['description'][0]
+			if "sn" in result[1]:
+				user["secondname"]=result[1]['sn'][0]
+			if "mail" in result[1]:
+				user["mail"]=result[1]['mail'][0]
+			if "canonicalName" in result[1]:
+				user["full_name"]=re.sub(r'.*/','',result[1]['canonicalName'][0])
+			users[user["account_name"]]=user
+
+		#	for key in result[1]:
+		#		print("%s = %s" %(key,result[1][key][0]))
+
+		#	for key in user:
+		#		print("user: %s = %s" % (key, user[key]))
+	return users
+
+#t=0
+#print("t=",t)
+#print("t=",t | 1 << 3)
+
+
+
+
+
--- user_ad_postgres_db.py
+++ user_ad_postgres_db.py
@@ -8,6 +8,72 @@ import psycopg2.extras
 import config as config
 import logger as log
 
+def get_ad_user_list():
+	try:
+		if config.DEBUG:
+			log.add("connect to: dbname='" + config.user_list_db_name + "' user='" +config.user_list_db_user + "' host='" + config.user_list_db_host + "' password='" + config.user_list_db_passwd + "'")
+		conn = psycopg2.connect("dbname='" + config.user_list_db_name + "' user='" +config.user_list_db_user + "' host='" + config.user_list_db_host + "' password='" + config.user_list_db_passwd + "'")
+		cur = conn.cursor()
+	except:
+		log.add("I am unable to connect to the database");return False
+	fio=familiya + " " + name + " " + otchestvo
+
+	try:
+		sql="""select
+			fio,
+			name,
+			familiya,
+			otchestvo,
+			login,
+			old_login,
+			passwd,
+			drsk_email,
+			drsk_email_passwd,
+			rsprim_email,
+			rsprim_email_passwd,
+			hostname,
+			ip,
+			os,
+			os_version,
+			patches,
+			doljnost,
+			add_time,
+			add_ip,
+			add_user_name
+		from ad_users
+		"""
+		if config.DEBUG:
+			log.add("sql=%s" % sql)
+		cur.execute(sql)
+		data = cur.fetchall()
+	except:
+		log.add("I am unable select data from db");return False
+	user_list={}
+	for line in data:
+		user={}
+		user["fio"]=line[0]
+		user["name"]=line[1]
+		user["familiya"]=line[2]
+		user["otchestvo"]=line[3]
+		user["login"]=line[4]
+		user["old_login"]=line[5]
+		user["passwd"]=line[6]
+		user["drsk_email"]=line[7]
+		user["drsk_email_passwd"]=line[8]
+		user["rsprim_email"]=line[9]
+		user["rsprim_email_passwd"]=line[10]
+		user["hostname"]=line[11]
+		user["ip"]=line[12]
+		user["os"]=line[13]
+		user["os_version"]=line[14]
+		user["patches"]=line[15]
+		user["doljnost"]=line[16]
+		user["add_time"]=line[17]
+		user["add_ip"]=line[18]
+		user["add_user_name"]=line[19]
+		user_list[user["login"]]=user
+	return user_list
+
 def add_ad_user(name, familiya, otchestvo, login, old_login, passwd, drsk_email, drsk_email_passwd, rsprim_email, rsprim_email_passwd, hostname, ip, os, os_version, patches, doljnost, add_ip, add_user_name):
 	try:
 		if config.DEBUG:
