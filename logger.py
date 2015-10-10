572d9df10e20608d9926bbc43ce1ee999ded33f8
progserega <progserega@gmail.com>
2015-10-10 16:08:34 +1000

    убрал вывод на экран логов

--- logger.py
+++ logger.py
@@ -5,11 +5,12 @@ import datetime
 from os import uname
 import config as conf
 
-def add(text):
+def add(text, debug=False):
   outtext = ''
   nowtime = datetime.datetime.now()
   outtext += '\n' + (str(nowtime.strftime("%Y-%m-%d %H:%M:%S")) + ": " + text.encode('utf8'))
-  print outtext,
+  if debug:
+	  print outtext,
   file_log_d = open(conf.log,'a')
   file_log_d.write(outtext+'\n')
   file_log_d.close()
