30943c45e91c24d487d16b378c218c4e3489e1e8
progserega <progserega@gmail.com>
2015-10-19 12:05:50 +1000

    поправил отправку почты

--- sendemail.py
+++ sendemail.py
@@ -9,34 +9,36 @@ from email.utils import COMMASPACE, formatdate
 from email import encoders
 import socket
 
-def sendmail(text="произошёл инцедент. Необходимо участие админа.", subj="важное сообщение", send_to='abuse@rsprim.ru' , server='172.21.254.5', port=25, send_from='root@redmine.rs.int', username='', password='', isTls=True, files=[] ):
-    msg = MIMEMultipart()
-    msg['From'] = send_from
+def sendmail2(text="произошёл инцедент. Необходимо участие админа.", subj="важное сообщение", send_to='abuse@rsprim.ru' , server='172.21.254.5', port=25, send_from='root@redmine.rs.int', username='', password='', isTls=True, files=[] ):
+	msg = MIMEMultipart()
+	msg['From'] = send_from
 	#msg['To'] = COMMASPACE.join(send_to)
-    msg['To'] = send_to
-    msg['Date'] = formatdate(localtime = True)
-    msg['Subject'] = subj
+	msg['To'] = send_to
+	msg['Date'] = formatdate(localtime = True)
+	msg['Subject'] = subj
 
 	#msg.attach( MIMEText(text) )
-    msg.attach( MIMEText(text, 'plain', 'UTF-8') )
+	msg.attach( MIMEText(text, 'plain', 'UTF-8') )
 
-    for f in files:
-        part = MIMEBase('application', "octet-stream")
-        part.set_payload( open(f,"rb").read() )
-        encoders.encode_base64(part)
-        part.add_header('Content-Disposition', 'attachment; filename="{0}"'.format(os.path.basename(f)))
-        msg.attach(part)
-
+	for f in files:
+		part = MIMEBase('application', "octet-stream")
+		part.set_payload( open(f,"rb").read() )
+		encoders.encode_base64(part)
+		part.add_header('Content-Disposition', 'attachment; filename="{0}"'.format(os.path.basename(f)))
+		msg.attach(part)
 	try:
 		smtp = smtplib.SMTP(server, port)
 		if isTls: smtp.starttls()
-#   	smtp.login(username,password)
+		#   	smtp.login(username,password)
 		smtp.sendmail(send_from, send_to, msg.as_string())
 		smtp.quit()
-	except SMTPException as e:
+	except:
 		return False 
 	return True
 		
 
 
-#sendmail(send_to="semenov@rsprim.ru",files=["README"])
+#if sendmail2(send_to="semenov@rsprim.ru") == False:
+#	print ("error")
+#else:
+#	print ("success")
