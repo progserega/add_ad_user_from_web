baf64441c314c5845c3d0db92b06760abbe24536
progserega <progserega@gmail.com>
2015-10-19 13:18:04 +1000

    кодировка

--- add_to_email_db.py
+++ add_to_email_db.py
@@ -45,7 +45,8 @@ def add_user_to_exim_db(db_host, db_name, db_user, db_passwd, email_prefix, emai
 			 % {\
 				 "username":mdb.escape_string(email_prefix + "@" + email_domain), \
 				 "password":mdb.escape_string(email_passwd), \
-				 "name":mdb.escape_string(email_descr), \
+				 "name":email_descr, \
+				 #"name":mdb.escape_string(email_descr), \
 				 "maildir":mdb.escape_string(email_domain + "/" + email_prefix + "/"), \
 				 "quota":"0", \
 				 "local_part":mdb.escape_string(email_prefix), \
