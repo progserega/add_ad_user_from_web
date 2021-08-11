dd6589c9b943b610fc1a6db81388ba4d007966b0
Семёнов Сергей <semenov@rsprim.ru>
2021-08-11 13:57:32 +1000

    поправил отступы

--- user_phone_from_site.py
+++ user_phone_from_site.py
@@ -20,29 +20,29 @@ def get_exception_traceback_descr(e):
   return result
 
 def get_users_phones_from_site():
-#  Начало 
-	log.add("get_users_phones_from_site()")
-	try:
-		con = mdb.connect(conf.user_phone_db_host, conf.user_phone_db_user, conf.user_phone_db_passwd, conf.user_phone_db_name);
-		cur = con.cursor()
-	except mdb.Error, e:
-		print "Error %d: %s" % (e.args[0],e.args[1])
-		log.add("error connect to mysql user phone db: %d: %s" % (e.args[0],e.args[1]))
-		sys.exit(1)
+  #  Начало 
+  log.add("get_users_phones_from_site()")
+  try:
+    con = mdb.connect(conf.user_phone_db_host, conf.user_phone_db_user, conf.user_phone_db_passwd, conf.user_phone_db_name);
+    cur = con.cursor()
+  except mdb.Error, e:
+    print "Error %d: %s" % (e.args[0],e.args[1])
+    log.add("error connect to mysql user phone db: %d: %s" % (e.args[0],e.args[1]))
+    sys.exit(1)
 
-	users_phones={}
-	sql="select name, phone, (select dolzh_name from ae_phone_dolzn where id=ae_phone_kadry.dolzh_id) as dolj, (select otdel_name from ae_phone_otdel where id=ae_phone_kadry.otdel1_id) as otdel,e_mail  from ae_phone_kadry"
-	try:
-		cur.execute('SET NAMES cp1251')
-		cur.execute(sql)
-		data = cur.fetchall()
-	except mdb.Error, e:
-		print "Error %d: %s" % (e.args[0],e.args[1])
-		log.add("error connect to mysql user phone db: %d: %s" % (e.args[0],e.args[1]))
-		sys.exit(1)
+  users_phones={}
+  sql="select name, phone, (select dolzh_name from ae_phone_dolzn where id=ae_phone_kadry.dolzh_id) as dolj, (select otdel_name from ae_phone_otdel where id=ae_phone_kadry.otdel1_id) as otdel,e_mail  from ae_phone_kadry"
+  try:
+    cur.execute('SET NAMES cp1251')
+    cur.execute(sql)
+    data = cur.fetchall()
+  except mdb.Error, e:
+    print "Error %d: %s" % (e.args[0],e.args[1])
+    log.add("error connect to mysql user phone db: %d: %s" % (e.args[0],e.args[1]))
+    sys.exit(1)
 
-	for item in data:
-		user={}
+  for item in data:
+    user={}
     try:
       fio=item[0].decode("cp1251").encode("utf-8")
       user["fio"]=fio
@@ -56,6 +56,6 @@ def get_users_phones_from_site():
       log.add("skip record for user with email: %s"item[4])
       continue
 
-	if con:    
-		con.close()
+  if con:    
+    con.close()
 	return users_phones
